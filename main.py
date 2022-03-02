a9a5c0c6efcdd2b08de48759c654817d6fae82c4
cosmin531 <98645907+cosmin531@users.noreply.github.com>
2022-03-02 17:41:14 +0200

    Update main.ts, main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+paharCirese=1
+paharPortocale=2
+paharGol=3
+basic.show_number(paharCirese)
+basic.show_number(paharPortocale)
+paharGol=paharCirese
+paharCirese=paharPortocale
+paharPortocale=paharGol
+basic.show_string("interschimbare")
+basic.show_number(paharCirese)
+basic.show_number(paharPortocale)
+nota1=5
+nota2=10
+
+
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let paharCirese = 1
+let paharPortocale = 2
+let paharGol = 3
+basic.showNumber(paharCirese)
+basic.showNumber(paharPortocale)
+paharGol = paharCirese
+paharCirese = paharPortocale
+paharPortocale = paharGol
+basic.showString("interschimbare")
+basic.showNumber(paharCirese)
+basic.showNumber(paharPortocale)
+let nota1 = 5
+let nota2 = 10
